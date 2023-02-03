5cccdd74e776471718808190dd0883aece2875a2
holmista <tornikebuchukuri@redberry.ge>
2023-02-04 02:15:19 +0400

    feat: add session class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-import express from "express";
+import express, { Request } from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import { User } from "@prisma/client";
+
+declare global {
+  namespace Express {
+    export interface Request {
+      user?: User;
+    }
+  }
+}
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res) => {
   res.end("Hello World");
+  req.user;
 });
 
 app.listen(3000, () => {
