35c5326a3cf7ebbca67c50d78674559ad56b049a
holmista <tornikebuchukuri@redberry.ge>
2023-02-09 01:27:10 +0400

    feat: get github and google tokens

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,13 +1,32 @@
-import { test, expect } from "@jest/globals";
+import { test, expect, jest } from "@jest/globals";
+import getGoogleCodePuppeteer from "../src/helpers/getGoogleCodePuppeteer.js";
+import getGithubCodePuppeteer from "../src/helpers/getGithubcodePuppeteer.js";
 
-import { GoogleAuth, GithubAuth } from "../src/controllers/auth.js";
-
-test("give error if invalid code is provided in google login", async () => {
-  const googleAuth = new GoogleAuth("123");
-  await expect(googleAuth.login()).rejects.toThrow("Something went wrong");
+import googleClient from "../src/configs/googleClient.js";
+import githubClient from "../src/configs/githubClient.js";
+jest.setTimeout(10000);
+test("return null if invalid code is provided in google get tokens", async () => {
+  const result = await googleClient.getTokens("123");
+  expect(result).toBeNull();
 });
 
-test("give error if invalid code is provided in github login", async () => {
-  const githubAuth = new GithubAuth("123");
-  await expect(githubAuth.login()).rejects.toThrow("Something went wrong");
+test("return null if invalid code is provided github get tokens", async () => {
+  const result = await githubClient.getTokens("123");
+  expect(result).toBeNull();
+});
+
+test("return access and id tokens if valid code is provided google get tokens", async () => {
+  const code = await getGoogleCodePuppeteer();
+  const result = await googleClient.getTokens(code);
+  expect(result).toHaveProperty("access_token");
+  expect(result?.access_token).toBeTruthy();
+  expect(result).toHaveProperty("id_token");
+  expect(result?.id_token).toBeTruthy();
+});
+
+test("return access and id tokens if valid code is provided github get tokens", async () => {
+  const code = await getGithubCodePuppeteer();
+  const result = await githubClient.getTokens(code);
+  expect(result).toHaveProperty("access_token");
+  expect(result?.access_token).toBeTruthy();
 });
